dcfc25c46506dc604205fb6f83c6bcde00a9d5f1
AshileySabah <ashileysabrina2002@gmail.com>
2026-08-17 20:16:56 -0300

    feat: specialties

---
+++ src/app/mocks/specialties.mock.ts
@@ -0,0 +1,9 @@
+import { Specialty } from '../core/models';
+
+export const SPECIALTIES_MOCK: Specialty[] = [
+  { id: 'sp-nutricao', name: 'Nutrição' },
+  { id: 'sp-psicologia', name: 'Psicologia' },
+  { id: 'sp-fisioterapia', name: 'Fisioterapia' },
+  { id: 'sp-fonoaudiologia', name: 'Fonoaudiologia' },
+  { id: 'sp-educacao-fisica', name: 'Educação Física' },
+];

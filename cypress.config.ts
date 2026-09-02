eb23e511057621e62047729e977d531db623d946
Yasmin Feliciano <713yasminlima@gmail.com>
2026-09-02 13:33:41 -0300

    Cypress configured

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+
+    setupNodeEvents(on, config) {
+      // Eventos do Cypress podem ser adicionados aqui futuramente.
+      return config;
+    },
+  },
+});
